7cb71256ab3feb3b24fef49b9f78b529f413dbd4
blackfoxgdl <ruben.alonso21@gmail.com>
2021-06-01 16:04:50 -0500

    new message

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
-        socket.emit('message', generateMessage('Admin', 'Welcome'));
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`));
+        socket.emit('message', generateMessage('Admin says:', 'Welcome'));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin says:', `${user.username} has joined!`));
         io.to(user.room).emit('roomData', {
             room: user.room,
             users: getUsersInRoom(user.room)
@@ -50,14 +50,14 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!');
         }
 
-        io.to(user.room).emit('message', generateMessage(user.username, message));
+        io.to(user.room).emit('message', generateMessage(`${user.username} says:`, message));
         callback();
     });
 
     socket.on('sendLocation', (coords, callback) => {
         const user = getUser(socket.id);
         console.log(user);
-        io.to(user.room).emit('shareLocation', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        io.to(user.room).emit('shareLocation', generateLocationMessage(`${user.username} says:`, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
 
         callback();
     });
@@ -66,7 +66,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id);
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`));
+            io.to(user.room).emit('message', generateMessage('Admin says:', `${user.username} has left!`));
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
